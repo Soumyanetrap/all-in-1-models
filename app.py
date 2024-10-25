3e7ea3b24c1b8f6b6925f9e820766e04e0c1e7c2
Soumyanetrap <soumyanetrap@gmail.com>
2024-10-25 16:51:35 +0530

    models server

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Sample data
+data = {
+    1: {'name': 'Item 1', 'description': 'This is item 1'},
+    2: {'name': 'Item 2', 'description': 'This is item 2'},
+}
+
+@app.route('/api/items', methods=['GET'])
+def get_items():
+    return jsonify(data)
+
+@app.route('/api/items/<int:item_id>', methods=['GET'])
+def get_item(item_id):
+    item = data.get(item_id)
+    if item:
+        return jsonify(item)
+    else:
+        return jsonify({'error': 'Item not found'}), 404
+
+@app.route('/api/items', methods=['POST'])
+def create_item():
+    new_id = max(data.keys()) + 1
+    new_item = request.json
+    data[new_id] = new_item
+    return jsonify({'id': new_id, **new_item}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
